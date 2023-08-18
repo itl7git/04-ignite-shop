de42b3ae6e3cc028c0ffb95ba3711370154cf70a
Italo Leal <itlcoorporativo@gmail.com>
2023-08-18 16:15:35 -0300

    Cria a estrutura da página de produtio

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,26 @@
+import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+
 export default function Product() {
+  const { query } = useRouter()
+  
   return (
-    <h1>Product</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, aliquid voluptas consequatur repudiandae voluptatibus at, magnam doloremque pariatur quam dolorem laboriosam modi corporis voluptatum veniam quaerat eveniet ipsum. Deleniti, expedita?</p>
+      
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
