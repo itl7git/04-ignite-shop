f5675873a20500064697e3444fb2727b7d3a5581
Italo Leal <itlcoorporativo@gmail.com>
2023-08-18 17:10:17 -0300

    Gerando a pagina com SSG e parametros dinâmicos

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,21 +1,38 @@
-import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product'
 import Image from 'next/image'
+import Stripe from 'stripe'
+import { stripe } from '@/lib/stripe'
+import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
 
-export default function Product() {
-  const { query } = useRouter()
-  
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string,
+    description: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter()
+
+  if(isFallback) {
+    return <p>Loading...</p>
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
-        
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, aliquid voluptas consequatur repudiandae voluptatibus at, magnam doloremque pariatur quam dolorem laboriosam modi corporis voluptatum veniam quaerat eveniet ipsum. Deleniti, expedita?</p>
+        <p>{product.description}</p>
       
         <button>
           Comprar agora
@@ -24,3 +41,38 @@ export default function Product() {
     </ProductContainer>
   )
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+   paths: [
+    { params: { id: 'prod_OTEa6ge79AScYV' } }
+   ],
+   fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params?.id
+  
+  const product = await stripe.products.retrieve(productId!, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount! / 100,),
+        description: product.description,
+      }
+    },
+    revalidate: 60 * 60 * 1, //1 hour
+  }
+} 
